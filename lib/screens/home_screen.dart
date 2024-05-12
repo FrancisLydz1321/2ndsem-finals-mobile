5b2076baa94c537d95d188b9e86da9a1e9900eed
Francis Lydzustre <francislydzustre1321@gmail.com>
2024-05-13 04:14:33 +0800

    6th attempt

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:_fourtho/gen/colors.gen.dart';
 import 'package:_fourtho/gen/fonts.gen.dart';
+import 'package:_fourtho/model/card_model.dart';
 import 'package:_fourtho/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
@@ -33,8 +34,9 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             const SizedBox(height: 10),
-            _SearchBar(),
+            _SearchBar(), // custom search widget
             const SizedBox(height: 10),
+            _CategoryFilters(), // custom widget
           ],
         ),
       ),
@@ -61,5 +63,47 @@ class _SearchBar extends StatelessWidget {
         onChanged: (value) {},
       ),
     );
+  }
+}
+
+class _CategoryFilters extends StatelessWidget {
+  const _CategoryFilters({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 30,
+      child: ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        children: [
+          ...CardCategory.values.map(
+            (category) => CustomChip(label: category.name),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomChip extends StatelessWidget {
+  final String label;
+  final bool isSelected;
+
+  const CustomChip({
+    Key? key,
+    required this.label,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(right: 10.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 5.0),
+        decoration: BoxDecoration(
+            color: isSelected ? ColorName.primaryColor : ColorName.disabledGrey,
+            borderRadius: BorderRadius.circular(50.0)),
+        child: Center(child: AppText.small(label)));
   }
 }
