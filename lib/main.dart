8cf4c04bf9c4a555e9f0b27ce2c75c128ea935d1
Francis Lydzustre <francislydzustre1321@gmail.com>
2024-05-12 23:48:53 +0800

    second attempt

--- lib/main.dart
+++ lib/main.dart
@@ -107,7 +107,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this maasdasdasdasdasny times:',
+              'You have pushed thasdasdasdasdasde button this maasdasdasdasdasny times:',
             ),
             Text(
               '$_counter',
